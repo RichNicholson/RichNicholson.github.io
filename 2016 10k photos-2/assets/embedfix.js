15898ed314106e30677dbe0baae986c45f93f6ed
Rich Nicholson <richnicholson@gmail.com>
2025-07-20 13:26:36 +0100

    Replace site content with Hugo-generated static site from err-website

--- 2016 10k photos-2/assets/embedfix.js
+++
@@ -1,9 +0,0 @@
-document.write("<NOSCRIPT class=fix>")
-
-window.onload = function() {
-  elems=document.getElementsByTagName("noscript");
-  for(i=elems.length-1;elem=elems[i];i--) {
-    if(elem.className=="fix")
-      elem.outerHTML = elem.innerHTML
-  }
-}
